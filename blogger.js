7254c6a3b2a31adcc605915a74add5b6b70eea0a
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-11 00:52:38 +0530

    Add all my files

--- blogger.js
+++ blogger.js
@@ -1,4 +1,3 @@
-//      header
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -25,24 +24,3 @@ window.onscroll = () =>{
   searchIcon.classList.remove('fa-times');
   searchForm.classList.remove('active');
 }
-// Fetcing data from api
-const getRandomUserData = () =>{
-    fetch(`https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09`)
-    .then((data) => data.json())
-    .then((data) => console.log(data));
-};
-const generateCards = (data) => {
-    const result =data.[0];
-
-    const userCard =`
-    <div class="cards>
-        <div class="left-side">
-            <h2>${result.</h2>
-        </div>
-        <div class="right-side">
-
-        </div>
-    </div>
-    `
-}
-getRandomUserData();
