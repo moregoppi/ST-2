8a0a245ebdfc8ed0176bd981fc5ebb3ed6af75b0
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-08-11 00:31:47 +0530

    Add all my files

--- blogger.js
+++ blogger.js
@@ -25,3 +25,11 @@ window.onscroll = () =>{
   searchIcon.classList.remove('fa-times');
   searchForm.classList.remove('active');
 }
+// Fetcing data from api
+const getRandomUserData = () =>{
+    fetch(`https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09`)
+    .then((data) => data.json())
+    .then((data) => console.log(data));
+};
+
+getRandomUserData();
